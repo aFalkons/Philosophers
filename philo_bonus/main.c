88764e1a5aa12108fd174b7b92ad8c99bde50345
Andrea Falconi <afalconi@e3r9p1.42roma.it>
2023-06-19 13:12:35 +0200

    semprestarting

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -5,12 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: afalconi <afalconi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 12:54:04 by afalconi          #+#    #+#             */
-/*   Updated: 2023/06/19 13:01:55 by afalconi         ###   ########.fr       */
+/*   Created: 2023/06/19 13:10:55 by afalconi          #+#    #+#             */
+/*   Updated: 2023/06/19 13:12:09 by afalconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "philo_bonus.h"
 
 int	main(int ac, char **av)
 {
