e5fa407ba7859627fcd8f31961f5589fadd40873
Andrea Falconi <afalconi@e4r10p3.42roma.it>
2023-06-19 19:40:03 +0200

    nonmevadefancazzo

--- philo/main.c
+++ philo/main.c
@@ -12,7 +12,51 @@
 
 #include "philo.h"
 
+
 int	main(int ac, char **av)
 {
+	struct timeval tv;
+	setup(ac - 1,&av[1]);
+	// pthread_t p1, p2, p3, p4;
+	// pthread_mutex_init(mutex, NULL);
+	// if (pthread_create(&p1, NULL, &routine, NULL) != 0) {
+	//     return 1;
+	// }
+	// if (pthread_join(p1, NULL) != 0) {
+	//     return 5;
+	// }
+	// pthread_mutex_destroy(&mutex);
+	// printf("Number of mails: %d\n", mails);
+	while(1)
+	{
+		gettimeofday(&tv, NULL);
+		printf("%ld\n", tv.tv_usec);
+	}
+}
 
+void	setup(int ac, char **av)
+{
+	int i;
+
+	i = -1;
+	while(++i < ac)
+	{
+		if (i != 5)
+			ckchar(&av[i], "a");
+		else
+			ckchar(&av[i], "0");
+	}
+	
+}
+
+void	ckchar(char	*av, char f)
+{
+	int	i;
+
+	i = -1;
+	while(av[++i])
+	{
+		if (!(av[i] > 48 && av[i] < 57) || av[i] == f)
+			printf("tumaadre\n");
+	}
 }
--- philo/philo.h
+++ philo/philo.h
@@ -19,6 +19,26 @@
 # include <sys/types.h>
 # include <unistd.h>
 # include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
 
+// typedef struct s_philo {
+// 	t_listphilo		*lphilo;
+// 	int				nphilo;
+// 	int				neat;
+// 	int				ttdie;
+// 	int				ttsleep;
+// 	int				tteat;
+// }	t_philo;
+
+// typedef struct s_listphilo {
+// 	pthread_t	*tr;
+// 	pthread_mutex_t fork;
+
+// }	t_listphilo;
+
+void*	routine();
 
 #endif
